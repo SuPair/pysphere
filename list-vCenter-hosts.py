1d1c9ded4719ded817dcaeb9c5a812bba52ab049
jidong9527 <doerji@126.com>
2016-04-27 17:14:18 +0800

    Create list-vCenter-hosts.py
    列出指定vCenter下面管理的所有主机，并将结果输出到hosts.txt中

---
+++ list-vCenter-hosts.py
@@ -0,0 +1,37 @@
+#_*_coding:utf-8_*_
+__author__ = 'jidong'
+
+from pysphere import VIServer
+import ssl
+
+vCenterIP = "192.168.10.100"                   
+username = "administrator@vsphere.local"
+password = "password"
+
+#######################################
+# Disable SSL verification in Python  #
+#######################################
+def DisableSSL():
+    try:
+        _create_unverified_https_context = ssl._create_unverified_context
+    except AttributeError:
+        # Legacy Python that doesn't verify HTTPS certificates by default
+        pass
+    else:
+        # Handle target environment that doesn't support HTTPS verification
+        ssl._create_default_https_context = _create_unverified_https_context
+#######################################
+
+DisableSSL()
+server = VIServer()
+server.connect(vCenterIP,username,password)
+hostslist = server.get_hosts().values()
+# print hostslist
+
+f = open("hosts.txt","w")
+sep = "\n"
+f.write(sep.join(hostslist))
+f.close()
+
+#Disconnecting from the server
+server.disconnect()
